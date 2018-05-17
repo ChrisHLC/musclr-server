6d6f62d2ad1092ae038c6c528108346636a28571
Zhening <zhening.yang@gmail.com>
2018-05-17 16:49:06 +0200

    update stalker nodes and edges json files

--- server/routes/node.route.js
+++ server/routes/node.route.js
@@ -2,29 +2,39 @@ const express = require('express');
 const nodeRouter = express.Router();
 const fs = require('fs');
 
-
 const {Node} = require('../models/node.model');
 const {Edge} = require('../models/edge.model');
 
-
-nodeRouter.get('/group/:group', async (req, res) => {
+nodeRouter.post('/filter', async (req, res) => {
     try {
-        const group = req.params.group;
+        const body = req.body;
+        const group = parseInt(body.group);
+        const minAge = parseInt(body.minAge);
+        const maxAge = parseInt(body.maxAge);
+        const gymId = parseInt(body.gymId);
+
         let nodes = [];
         let edges = [];
-        if(group === '99'){
-            nodes = await Node.find({});
-            edges = await Edge.find({});
+
+        if (group === -1) {
+            if (gymId === -1) {
+                nodes = await Node.find({age: {$gte: minAge, $lte: maxAge}});
+            } else {
+                nodes = await Node.find({age: {$gte: minAge, $lte: maxAge}, salle: gymId});
+            }
         } else {
-            nodes = await Node.find({group: group});
-
-            let nodeIds = [];
-            nodes.forEach(n => {
-                nodeIds.push(n.id);
-            });
-
-            edges = await Edge.find({$and: [{source: {$in: nodeIds}}, {target: {$in: nodeIds}}]});
+            if (gymId === -1) {
+                nodes = await Node.find({group: group, age: {$gte: minAge, $lte: maxAge}});
+            } else {
+                nodes = await Node.find({group: group, age: {$gte: minAge, $lte: maxAge}, salle: gymId});
+            }
         }
+
+        let nodeIds = [];
+        nodes.forEach(n => {
+            nodeIds.push(n.id);
+        });
+        edges = await Edge.find({$and: [{source: {$in: nodeIds}}, {target: {$in: nodeIds}}]});
 
         res.send({nodes, edges});
 
@@ -32,5 +42,73 @@ nodeRouter.get('/group/:group', async (req, res) => {
         res.status(400).send(e);
     }
 });
+
+//
+// nodeRouter.get('/group/:group', async (req, res) => {
+//     try {
+//         const group = req.params.group;
+//         let nodes = [];
+//         let edges = [];
+//         if (group === '99') {
+//             nodes = await Node.find({});
+//             edges = await Edge.find({});
+//         } else {
+//             nodes = await Node.find({group: group});
+//
+//             let nodeIds = [];
+//             nodes.forEach(n => {
+//                 nodeIds.push(n.id);
+//             });
+//             edges = await Edge.find({$and: [{source: {$in: nodeIds}}, {target: {$in: nodeIds}}]});
+//         }
+//
+//         res.send({nodes, edges});
+//
+//     } catch (e) {
+//         res.status(400).send(e);
+//     }
+// });
+//
+// nodeRouter.get('/gym/:gym', async (req, res) => {
+//     try {
+//         const gym = parseInt(req.params.gym);
+//
+//         const nodes = await Node.find({salle: gym});
+//
+//         let nodeIds = [];
+//         nodes.forEach(n => {
+//             nodeIds.push(n.id);
+//         });
+//         const edges = await Edge.find({$and: [{source: {$in: nodeIds}}, {target: {$in: nodeIds}}]});
+//
+//
+//         res.send({nodes, edges});
+//
+//     } catch (e) {
+//         res.status(400).send(e);
+//     }
+// });
+//
+// nodeRouter.get('/age/:min/:max', async (req, res) => {
+//     try {
+//         const ageMin = parseInt(req.params.min);
+//         const ageMax = parseInt(req.params.max);
+//
+//         const nodes = await Node.find({
+//             age: {$gte: ageMin, $lte: ageMax}
+//         });
+//
+//         let nodeIds = [];
+//         nodes.forEach(n => {
+//             nodeIds.push(n.id);
+//         });
+//         const edges = await Edge.find({$and: [{source: {$in: nodeIds}}, {target: {$in: nodeIds}}]});
+//
+//         res.send({nodes, edges});
+//
+//     } catch (e) {
+//         res.status(400).send(e);
+//     }
+// });
 
 module.exports = nodeRouter;
